54e3cc3811ffaf8e40411b9d797638c8422e9888
Wendell Valdez <vinduhl@gmail.com>
2016-10-18 20:00:24 -0500

    updates

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
+
+const port = 8800;
+
+const messages = [];
+
+app.options("/", (req, res) => {
+  res.status(200).set( {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  })
+  .send();
+})
+
+app.get("/", (req, res) => {
+  res.status(200).set( {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  })
+  .send(JSON.stringify(messages));
+})
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  messages.push( {
+    message: req.body.message,
+    time: new Date()
+  });
+  res.status(200).set( {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  })
+  .send(JSON.stringify(messages));
+})
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
